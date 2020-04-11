37f571d37afab93e8ffdacddd69ef08099c2793b
giova831 <gololeal@hotmail.com>
2020-04-10 23:33:15 -0500

    mi segundo commit

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 'use strict'
 
-var express = require ('express');
-var bodyParser = require('body-parser');
-var app = express();
+var app = require('./app');
 var port = process.env.PORT || 3678;
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-app.get('/prueba/:nombre?', function(req, res){
-
-    if (req.params.nombre){
-        var nombre = req.params.nombre;
-    }
-    else{
-        var nombre = "SIN NOMBRE";
-    }
-    
-
-    res.send({texto: "hola mundo con nodejs y express " + nombre})
-});
-
 app.listen(port, function(){
     console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
     console.log("PRUEBA NODEMON");
-})
+});
